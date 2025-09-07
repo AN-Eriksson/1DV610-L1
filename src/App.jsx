c0ce407aa1f778db9acfacb40e9d7a180b28a01d
Andreas Eriksson <ae225ak@student.lnu.se>
2025-09-07 19:21:41 +0200

    Add RobberLanguage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import { InputForm } from './components/InputForm'
 import { NameDisplay } from './components/NameDisplay'
+import { RobberLanguage } from './components/RobberLanguage'
 
 function App() {
   const [name, setName] = useState("")
   const [nameSubmitted, setNameSubmitted] = useState(false)
 
   return (
-    <div className='min-h-screen flex items-center justify-center bg-gray-300'>
+    <div className='min-h-screen flex flex-col items-center justify-center bg-gray-300'>
       {!nameSubmitted && (
         <InputForm
           name={name}
@@ -17,7 +18,10 @@ function App() {
       )}
 
       {nameSubmitted && (
-        <NameDisplay name={name} />
+        <>
+          <NameDisplay name={name} />
+          <RobberLanguage name={name} />
+        </>
       )}
 
     </div>
